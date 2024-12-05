cefb0e1bab00bcddfc0b9121071225066d9a7bcb
CarlesRa <jcramos8282@gmail.com>
2024-12-05 23:04:07 +0100

    reto05 3 stars

---
+++ reto05.js
@@ -0,0 +1,51 @@
+/**
+ * @param {{ type: 'I' | 'R', size: number }[]} shoes
+ * @returns {number[]} Available shoes 
+ */
+function organizeShoes(shoes) {
+  const TYPE_I = 'I'
+  const TYPE_R = 'R'
+  const result = []
+  const group = Object.groupBy(shoes, ({ type }) => type)  
+  const hasPropertyY = TYPE_I in group
+  if (hasPropertyY) {
+    Object.values(group[TYPE_I]).forEach(({ size }) => {
+      const index = group[TYPE_R].findIndex(p => p?.size === size);
+      if (index !== -1) {
+        result.push(size)
+        group[TYPE_R][index] = null
+      }
+    })
+  }
+  return result    
+}
+
+const shoes = [
+  { type: 'I', size: 38 },
+  { type: 'R', size: 38 },
+  { type: 'R', size: 42 },
+  { type: 'I', size: 41 },
+  { type: 'I', size: 42 }
+]
+
+console.log(organizeShoes(shoes))
+
+const shoes2 = [
+  { type: 'I', size: 38 },
+  { type: 'R', size: 38 },
+  { type: 'I', size: 38 },
+  { type: 'I', size: 38 },
+  { type: 'R', size: 38 }
+]
+
+console.log(organizeShoes(shoes2));
+
+const shoes3 = [
+  { type: 'I', size: 38 },
+  { type: 'R', size: 36 },
+  { type: 'R', size: 42 },
+  { type: 'I', size: 41 },
+  { type: 'I', size: 43 }
+]
+
+console.log(organizeShoes(shoes3))
