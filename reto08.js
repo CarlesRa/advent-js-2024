4488cde2dab5fb7e4906b76e55874e9ee1ab5229
CarlesRa <jcramos8282@gmail.com>
2024-12-08 11:09:24 +0100

    reto08 3 stars

--- reto08.js
+++ reto08.js
@@ -4,19 +4,27 @@
  * @returns {string} The reno race
  */
 function drawRace(indices, length) {
-  const lane = '~'
-  const reindeer = 'r'
-  let result = '';
+  return indices
+    .map((indice, index) => {
+      const position = indice >= 0 ? indice : length + indice;
+      const repeats = indices.length - (index + 1);
+      const lanes = '~'.repeat(length)
+        .split('')
+        .map((char, i) => (i === position ? (indice === 0 ? '~' : 'r') : char))
+        .join('');
+      return `${' '.repeat(repeats)}${lanes} /${index + 1}`;
+    })
+    .join('\n');
+  /* let result = '';
   indices.forEach((indice, index) => {
     const position = indice >= 0 ? indice : length + indice
     const repeats = indices.length - (index + 1)
-    let lanes = lane.repeat(length)
-    lanes = lanes.slice(0, position) + (indice === 0 ? lane : reindeer) + lanes.slice(position + 1)
+    let lanes = '~'.repeat(length)
+    lanes = lanes.slice(0, position) + (indice === 0 ? '~' : 'r') + lanes.slice(position + 1)
     result +=
       `${repeats > 0 ? ' '.repeat(repeats) : ''}${lanes} /${index + 1}${index < indices.length - 1 ? '\n' : ''}`
   })
-  console.log(result)
-  return result
+  return result */
 }
 
 //drawRace([0, 5, -3], 10)
@@ -34,7 +42,7 @@ function drawRace(indices, length) {
 ~~~~~r~~ /4
 */
 
-drawRace([3, 7, -2], 12)
+console.log(drawRace([3, 7, -2], 12))
 /*
   ~~~r~~~~~~~~ /1
  ~~~~~~~~r~~~ /2
