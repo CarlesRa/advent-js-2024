c21149fa43e9ddf573a884372c010c37dd830a3f
CarlesRa <jcramos8282@gmail.com>
2024-12-08 11:29:51 +0100

    reto08 4 stars

--- reto08.js
+++ reto08.js
@@ -4,27 +4,13 @@
  * @returns {string} The reno race
  */
 function drawRace(indices, length) {
-  return indices
-    .map((indice, index) => {
-      const position = indice >= 0 ? indice : length + indice;
-      const repeats = indices.length - (index + 1);
-      const lanes = '~'.repeat(length)
-        .split('')
-        .map((char, i) => (i === position ? (indice === 0 ? '~' : 'r') : char))
-        .join('');
-      return `${' '.repeat(repeats)}${lanes} /${index + 1}`;
-    })
-    .join('\n');
-  /* let result = '';
-  indices.forEach((indice, index) => {
-    const position = indice >= 0 ? indice : length + indice
-    const repeats = indices.length - (index + 1)
-    let lanes = '~'.repeat(length)
-    lanes = lanes.slice(0, position) + (indice === 0 ? '~' : 'r') + lanes.slice(position + 1)
-    result +=
-      `${repeats > 0 ? ' '.repeat(repeats) : ''}${lanes} /${index + 1}${index < indices.length - 1 ? '\n' : ''}`
-  })
-  return result */
+  return indices.map((indice, index) => {
+    const laneRepeats = indice >= 0 ? indice : length + indice;
+    const spaceRepeats = indices.length - index - 1;
+    const laneContent =
+      `${'~'.repeat(laneRepeats)}${indice === 0 ? '~' : 'r'}${'~'.repeat(length - laneRepeats - 1)}`;
+    return `${' '.repeat(spaceRepeats)}${laneContent} /${index + 1}`;
+  }).join('\n');
 }
 
 //drawRace([0, 5, -3], 10)
