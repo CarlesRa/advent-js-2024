5eb934c1d4d8d7b4de0e114d367a9f03aacd69cc
CarlesRa <jcramos8282@gmail.com>
2024-12-12 20:01:27 +0100

    reto12 4 stars

---
+++ reto12.js
@@ -0,0 +1,40 @@
+/** @param {string} ornaments
+ * @return {number} - The price of the tree
+ */
+function calculatePrice(ornaments) {
+  const checker = { '*': 1, 'o': 5, '^': 10, '#': 50, '@': 100}
+  let oldValue
+  let result = 0
+  for (let i = (ornaments.length - 1); i >= 0; i--) {
+    const ornament = ornaments.charAt(i)
+    const value = checker[ornament]
+    if (!value) {
+      return value
+    }
+    oldValue && oldValue > value ? result -= value : result += value
+    oldValue = value
+  }
+  return result
+}
+
+/*
+
+  *: Copo de nieve - Valor: 1
+  o: Bola de Navidad - Valor: 5
+  ^: Arbolito decorativo - Valor: 10
+  #: Guirnalda brillante - Valor: 50
+  @: Estrella polar - Valor: 100
+
+*/
+
+console.log(calculatePrice('***'))  // 3   (1 + 1 + 1)
+console.log(calculatePrice('*o'))   // 4   (5 - 1)
+console.log(calculatePrice('o*'))   // 6   (5 + 1)
+console.log(calculatePrice('*o*'))  // 5  (-1 + 5 + 1)
+console.log(calculatePrice('**o*')) // 6  (1 - 1 + 5 + 1)
+console.log(calculatePrice('o***')) // 8   (5 + 3)
+console.log(calculatePrice('*o@'))  // 94  (-5 - 1 + 100)
+console.log(calculatePrice('*#'))   // 49  (-1 + 50)
+console.log(calculatePrice('@@@'))  // 300 (100 + 100 + 100)
+console.log(calculatePrice('#@'))   // 50  (-50 + 100)
+console.log(calculatePrice('#@Z'))  // undefined (Z es desconocido)
